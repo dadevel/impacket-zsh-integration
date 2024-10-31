a8ffb0aa74c57e0d35ed264c400b379eecf85a55
dadevel <dadevel@disroot.org>
2024-10-31 13:18:16 +0100

    update icons to latest nerdfonts

--- powerlevel10k.zsh
+++ powerlevel10k.zsh
@@ -1,9 +1,9 @@
 function prompt_krbconf() {
     if [[ -e "$KRB5CCNAME" && -n "$KRB5CCNAME_DOMAIN" && -n "$KRB5CCNAME_USER" ]]; then
         if [[ -n "$KRB5CCNAME_HOST" ]]; then
-            p10k segment -f 4 -i '' -t "$KRB5CCNAME_DOMAIN/$KRB5CCNAME_USER@$KRB5CCNAME_HOST"
+            p10k segment -f 4 -i $'\xef\x80\x87' -t "$KRB5CCNAME_DOMAIN/$KRB5CCNAME_USER@$KRB5CCNAME_HOST"
         else
-            p10k segment -f 4 -i '' -t "$KRB5CCNAME_DOMAIN/$KRB5CCNAME_USER"
+            p10k segment -f 4 -i $'\xef\x80\x87' -t "$KRB5CCNAME_DOMAIN/$KRB5CCNAME_USER"
         fi
     fi
 }
@@ -18,10 +18,16 @@ function prompt_proxyconf() {
         declare -r proxy=''
     fi
     if [[ -n "${netns}" && -n "${proxy}" ]]; then
-        p10k segment -f 2 -i '' -t "${proxy}@${netns}"
+        p10k segment -f 2 -i $'\xf3\xb0\x81\x95' -t "${proxy}@${netns}"
     elif [[ -n "${netns}" ]]; then
-        p10k segment -f 2 -i '' -t "${netns}"
+        p10k segment -f 2 -i $'\xf3\xb0\x81\x95' -t "${netns}"
     elif [[ -n "${proxy}" ]]; then
-        p10k segment -f 2 -i '' -t "${proxy}"
+        p10k segment -f 2 -i $'\xf3\xb0\x81\x95' -t "${proxy}"
+    fi
+}
+
+function prompt_tunnel() {
+    if ip link show tun0 &> /dev/null; then
+        p10k segment -f 1 -i $'\xf3\xb0\x81\x95' -t 'tun0'
     fi
 }
